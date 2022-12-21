6bab8c2f1f6716058927982536c402e198189151
markiegaming <58847729+markiegaming@users.noreply.github.com>
2022-12-21 13:47:50 +0100

    movement

--- chicken quest/Assets/ThirdPersonMovement.cs
+++ chicken quest/Assets/ThirdPersonMovement.cs
@@ -9,15 +9,34 @@ public class ThirdPersonMovement : MonoBehaviour
 
    public float speed = 6f;
    public float turnSmoothTime = 0.1f;
-   float turnSmoothVelocity;
+   float turnSmoothVelocity; 
+   Vector3 velocity;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    public float jumpHeight = 3f;
+
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+      isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+      if(isGrounded && velocity.y < 0) {
+        Debug.Log("hit ground");
+          velocity.y = -2f;
+      }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
+
         float vertical = Input.GetAxisRaw("Vertical"); 
+
        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
        if(direction.magnitude >= 0.1f) {
+
         float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
         transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
@@ -25,5 +44,11 @@ public class ThirdPersonMovement : MonoBehaviour
         Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;  
          controller.Move(moveDir.normalized * speed * Time.deltaTime);
        } 
+       if(Input.GetButtonDown("Jump") && isGrounded) {
+           velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+       }
+
+       velocity.y += gravity * Time.deltaTime;
+      controller.Move(velocity * Time.deltaTime);
     }
 }
